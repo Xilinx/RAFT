4276d71f30b4c40b6e44d23d33c962ded830e683
Sree Parvathi Anish <sreanish@amd.com>
2024-08-02 13:46:59 +0100

    pmtool: update csv format feature
      - add output filename
      - add timestamp to csv file
    
    Signed-off-by: Sree Parvathi Anish <sreanish@amd.com>

--- examples/python/pmtool/pm-cmd.py
+++ examples/python/pmtool/pm-cmd.py
@@ -4,44 +4,53 @@
 __author__ = "Salih Erim, Sree Parvathi Anish"
 __copyright__ = "Copyright 2023-2024, Advanced Micro Devices, Inc."
 
+import os
 import sys
 import json
 import argparse
 import csv
 import time
+import datetime
 sys.path.append('/usr/share/raft/xclient/raft_services')
 from pm_client import PM_Client
 
-# Specify the CSV file to write
-csv_file = '/home/petalinux/pm-getvaluesall-output.csv'
-
-def update_csv_data(csv_data, data):
+def update_csv_data(csv_data, data, sample_rate, duration):
     # Convert the dictionary to a list
-    flat_data = []
+    columns_order = []
+    row_item = {}
+    write_timestamp = 1
     for pd in data["VCK190"]:
         for key, value in pd.items():
-            total = value["Total Power"]
             for rails_item in value["Rails"]:
                 for rails_key, rails_value in rails_item.items():
-                    row_item = {"Rails": rails_key, "Power Domain": key}
-                    row_item.update(rails_value)
-                    row_item["Total Power"] = total
-                    flat_data.append(row_item)
-    csv_data.append(flat_data)
-    return csv_data
+                    if write_timestamp == 1:
+                        columns_order.append("Timestamp")
+                        start_time = datetime.datetime.now()
+                        row_item.update({"Timestamp" : start_time})
+                        write_timestamp = 0
+                    column_name = f"{key}-{rails_key}-{list(rails_value.keys())[0]}"
+                    columns_order.append(column_name)
+                    row_item.update({f"{column_name}": rails_value['Voltage']})
+                    column_name = f"{key}-{rails_key}-{list(rails_value.keys())[1]}"
+                    columns_order.append(column_name)
+                    row_item.update({f"{column_name}": rails_value['Current']})
+                    column_name = f"{key}-{rails_key}-{list(rails_value.keys())[2]}"
+                    columns_order.append(column_name)
+                    row_item.update({f"{column_name}": rails_value['Power']})
+        column_name = f"{key}-TotalPower"
+        columns_order.append(column_name)
+        row_item.update({f"{column_name}": value["Total Power"]})
+    csv_data.append(row_item)
+    return csv_data, columns_order
 
-def pm_csv_dump(csv_data):
-    # Specify the order of the columns
-    columns_order = ["Power Domain", "Rails", "Voltage", "Current", "Power", "Total Power"]
-
+def pm_csv_dump(csv_data, columns_order, filepath, filename):
+    csv_file = f"{filepath}/{filename}"
     try:
         with open(csv_file, 'w') as csvfile:
             csv_writer = csv.DictWriter(csvfile, fieldnames=columns_order, lineterminator='\n')
             csv_writer.writeheader()
-            for data in csv_data:
-                for row in data:
+            for row in csv_data:
                     csv_writer.writerow(row)
-                csv_writer.writerow({})
             print(f"The requested data is written to file - {csv_file}")
     except IOError:
         print(f"I/O error while opening {csv_file} to write")
@@ -112,33 +121,42 @@ def main():
             target_parser = argparse.ArgumentParser(description="Power Management Client Module CLI")
             target_parser.add_argument("-d", "--duration", required=True, help="Specify the duration in seconds")
             target_parser.add_argument("-s", "--samplingrate", required=True, help="Specify the sampling rate")
+            target_parser.add_argument("-o", "--outputfilepath", default='/home/petalinux', help="Specify the path to write the file")
             target_args, target_remining_args = target_parser.parse_known_args(remaining_args)
             print(f"target_args.duration = {target_args.duration}, target_remining_args.samplingrate = {target_args.samplingrate}")
             sample_rate = int(target_args.samplingrate)
+            filepath = target_args.outputfilepath
+            duration = int(target_args.duration)
             # Check the input arguments
             if (sample_rate < 1 or sample_rate > 12):
                 print(f"sample rate should be between 1 and 12")
                 sys.exit(1)
-            if (int(target_args.duration) <= 0):
+            if (duration <= 0):
                 print(f"duration should be a positive value")
                 sys.exit(1)
+            if not os.path.exists(filepath):
+                print(f"The path {filepath} is valid.")
+                sys.exit(1)
+
             # Find the sleep time from the sample rate
             sleeptime = 1/sample_rate
 
             csv_data = []
             # Mark the start time
+            start_datetime = datetime.datetime.now()
             start_time = time.time()
             while True:
                 operation_start_time = time.time()
                 values = client.GetValuesAll()
-                csv_data = update_csv_data(csv_data, values)
+                csv_data, columns_order = update_csv_data(csv_data, values, sample_rate, duration)
                 operation_exec_time = time.time() - operation_start_time
                 # If operation execution time is higher than sleep time, no need to sleep
                 if (sleeptime - operation_exec_time) > 0:
                     time.sleep(sleeptime -  operation_exec_time)
-                if time.time() - start_time >= int(target_args.duration):
+                if time.time() - start_time >= duration:
                     break
-            pm_csv_dump(csv_data)
+            filename = start_datetime.strftime(f"pmtool_s-{sample_rate}_d-{duration}_%Y-%m-%d_%H-%M-%S.csv")
+            pm_csv_dump(csv_data, columns_order, filepath, filename)
         
         else:
             args = parser.parse_args(remaining_args)
